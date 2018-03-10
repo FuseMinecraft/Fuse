98d6dff6ae74d2c2eba34ee427c284a92efb462f
Telesphoreo <craftyswans@gmail.com>
2018-03-09 21:20:10 -0800

    Fuse 2.2.1.60
    Bug fixes

--- src/main/java/us/flowdesigns/commands/Command_website.java
+++ src/main/java/us/flowdesigns/commands/Command_website.java
@@ -12,7 +12,7 @@ public class Command_website extends BaseCommand {
     public boolean run(final CommandSender sender, final Player sender_p, final Command cmd, final String commandLabel, final String[] args, final boolean senderIsConsole)
     {
         String website = plugin.getConfig().getString("server.website");
-        String dev = plugin.getConfig().getString("server.website");
+        String dev = plugin.getConfig().getString("server.dev");
         if (!website.equalsIgnoreCase("none"))
         {
         sender.sendMessage(ChatColor.AQUA + "Website: " + website);
